0ecd70f288426dac9df0f88126a61d849ba366e6
cfgarciaq <cfgarciaq@gmail.com>
2024-05-16 14:27:43 -0500

    Update app.js
    added query selector to obtain button reference
    
    added class list toggle to switch between light and dark classes
    
    added  logic to switch button name text according to active theme
    
    Add print to console

--- SimpleWebsite/app.js
+++ SimpleWebsite/app.js
@@ -11,3 +11,41 @@
 */
 
 'use stric';
+
+//adding an EventHandler
+
+/*An event handler is a way to run a JavaScript 
+function when an event happens on the page
+*/
+
+//use document.querySelector to get the button reference.
+const switcher = document.querySelector('.btn');
+//switcher is now a reference to the button in the page.
+
+switcher.addEventListener('click', function(){
+
+    /*
+    the toggle method is used to modify the <body> element's class 
+    attribute. This method automatically adds or removes 
+    the light-theme and dark-theme classes. 
+    
+    This code applies the dark styles instead of light styles 
+    on click, and then light styles instead of dark 
+    if you click again.    
+    */
+
+    document.body.classList.toggle('light-theme');    
+    document.body.classList.toggle('dark-theme');
+
+    
+    //retrieve active body class name to change btn txt
+    const className = document.body.className;
+    if(className == "light-theme") {
+        this.textContent = "Dark";
+    } else {
+        this.textContent = "Light";
+    }
+
+    //print to console the active class
+    console.log('current class name: ' + className);
+});
