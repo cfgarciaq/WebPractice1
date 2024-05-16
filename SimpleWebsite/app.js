5860b629649827da5027e4350c3be0ac3dd5d077
cfgarciaq <cfgarciaq@gmail.com>
2024-05-16 13:58:10 -0500

    Update app.js
    added strict mode to prevent unwanted global variablesand get better errors

---
+++ SimpleWebsite/app.js
@@ -0,0 +1,13 @@
+/*
+    From Microsoft:
+    JavaScript was designed to be easy to learn 
+    and allows certain mistakes to be made by the developer. 
+    For example, JavaScript doesn't throw an error 
+    when you use a misspelled variable, and instead creates 
+    a new global one. When you start learning JavaScript, 
+    having fewer errors is convenient. However, it can lead 
+    to writing code that is harder for browsers to optimize 
+    and harder for you to debug.
+*/
+
+'use stric';
